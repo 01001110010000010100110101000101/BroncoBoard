e6bb88d435e76b1a5c16080abd4228192dad2265
cheripai <datdo@rocketmail.com>
2015-10-22 16:56:02 -0700

    Change ArrayList to Set for getClassNames to avoid duplicates

--- src/main/java/edu/csupomona/cs480/ClassScraper.java
+++ src/main/java/edu/csupomona/cs480/ClassScraper.java
@@ -1,7 +1,8 @@
 package edu.csupomona.cs480;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -13,10 +14,10 @@ public class ClassScraper {
 
 	private static String url = "http://catalog.cpp.edu/preview_program.php?catoid=4&poid=983&returnto=751";
 	
-	public ArrayList<String> getClassNames() throws IOException {
+	public Set<String> getClassNames() throws IOException {
 		Document doc = Jsoup.connect(url).get();
 		Elements links = doc.select("li > span > a[href]");
-		ArrayList<String> classNames = new ArrayList<String>();
+		Set<String> classNames = new HashSet<String>();
 		for(Element link: links) {
 			String title = link.text();
 			String department = title.substring(0, title.indexOf(" "));
@@ -30,7 +31,7 @@ public class ClassScraper {
 	public static void main(String[] args) {
 		ClassScraper scrape = new ClassScraper();
 		try {
-            ArrayList<String> classNames = scrape.getClassNames();
+            Set<String> classNames = scrape.getClassNames();
             System.out.print(classNames);
 		} catch (IOException e) {
 			System.out.println("Caught IOException");
