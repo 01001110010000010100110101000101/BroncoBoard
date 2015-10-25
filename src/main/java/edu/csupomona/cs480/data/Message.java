14e7648b99c30d2c8f91cffa2d395d0e1136ab67
Sang Choi <akasyashc@gmail.com>
2015-10-25 16:52:10 -0700

    Added a model class for storing message

---
+++ src/main/java/edu/csupomona/cs480/data/Message.java
@@ -0,0 +1,34 @@
+package edu.csupomona.cs480.data;
+
+public class Message {
+	
+	private String name;
+	private String message;
+	private String time;
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getMessage(){
+		return message;
+	}
+	
+	public void setMessage(String message){
+		this.message = message;
+	}
+	
+	public String getTime(){
+		return time;
+	}
+	
+	public void setTime(String time){
+		this.time = time;
+	}
+
+
+}
