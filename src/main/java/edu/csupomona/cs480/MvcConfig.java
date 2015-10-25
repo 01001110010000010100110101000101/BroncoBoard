75c8d6f0c30016cabd38f3fd91c7b06b55c706b4
Kevin Ali <kevinali50@yahoo.com>
2015-10-25 14:30:09 -0700

    for some reason certain file where not getting pushed hopefully this doesnt fuck it up

---
+++ src/main/java/edu/csupomona/cs480/MvcConfig.java
@@ -0,0 +1,15 @@
+package edu.csupomona.cs480;
+//This manages what certain pages will be used when we try to go to that page
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
