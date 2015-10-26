3d66392bf4b1cdc6f60ed64eaa2391cc461c43e6
Sang Choi <akasyashc@gmail.com>
2015-10-25 18:41:07 -0700

    Fixed an issue that caused the program to crash
    I tested it out on my end but if it causes problem for any of you then
    let me know

---
+++ src/main/java/edu/csupomona/cs480/data/dao/UserDao.java
@@ -0,0 +1,37 @@
+package edu.csupomona.cs480.data.dao;
+
+import java.util.List;
+import edu.csupomona.cs480.data.User;
+import edu.csupomona.cs480.data.dao.UserDaoInterface;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+
+
+/**
+ * User Data Access Object (DAO)
+ * @author Sang Choi
+ */
+public class UserDao extends HibernateDaoSupport implements UserDaoInterface {
+
+	public void save(User user) {
+		getHibernateTemplate().save(user);
+		
+	}
+
+	public void update(User user) {
+		getHibernateTemplate().update(user);
+		
+	}
+
+	public void delete(User user) {
+		getHibernateTemplate().delete(user);
+		
+	}
+
+	public User getUserById(String userID) {
+		List list = getHibernateTemplate().find("from User where id=?", userID);
+		
+		return (User)list.get(0);
+	}
+
+}
