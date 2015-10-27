65736b0644a8c7022766b5c53704886707b1829d
cheripai <datdo@rocketmail.com>
2015-10-27 15:25:40 -0700

    Add test case for ClassScraper

---
+++ src/test/java/edu/csupomona/cs480/ClassScraperTest.java
@@ -0,0 +1,21 @@
+package edu.csupomona.cs480;
+
+import java.util.ArrayList;
+import org.junit.Test;
+import org.junit.Assert;
+
+
+public class ClassScraperTest {
+	
+	@Test
+	public void testGetClassNames() {
+		ClassScraper cs = new ClassScraper();
+		try {
+            ArrayList<String> classes = cs.getClassNames();
+            Assert.assertNotNull(classes);
+		} catch (Exception e) {
+			System.out.println("Exception caught while testing ClassScraper");
+		}
+	}
+
+}
