21370131175dd63f610792365f6276a01c24b45d
cheripai <datdo@rocketmail.com>
2015-10-26 17:54:36 -0700

    Add functionality to tabs and frontend for chat window

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,6 +1,18 @@
 (function() {
     var app = angular.module('BroncoBoard', []);
 
+    app.controller('TabController', function() {
+        this.tab = 0;
+
+        this.setTab = function(index) {
+            this.tab = index;
+        };
+
+        this.isSet = function(index) {
+            return this.tab === index;
+        };
+    });
+
     app.controller('BoardsController', ['$http', function($http) {
         var boards = this;
         boards.classes = [];
@@ -12,5 +24,9 @@
             }
         };
     }]);
+
+    app.controller('MessagingController', function() {
+
+    });
 
 })();
