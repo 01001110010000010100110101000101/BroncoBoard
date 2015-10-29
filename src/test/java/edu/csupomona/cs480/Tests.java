b11e81325f1f8bcb899b30bbfd11d546ac589f6e
Kevin Ali <kevinali50@yahoo.com>
2015-10-28 21:33:10 -0700

    My test file for assigment 6

---
+++ src/test/java/edu/csupomona/cs480/Tests.java
@@ -0,0 +1,17 @@
+package edu.csupomona.cs480;
+
+import java.util.ArrayList;
+import org.junit.Test;
+import org.junit.Assert;
+
+
+public class Tests {
+	
+	@Test
+	public void testGetStringURL() {
+		ClassScraper cs = new ClassScraper();
+		String url=cs.getURL();
+		
+	}
+
+}
