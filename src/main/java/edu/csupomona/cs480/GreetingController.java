e22bc436e9182ad9d610439023b3169de16e2f00
Arthur Le <arthurle9@gmail.com>
2015-11-01 03:04:32 -0800

    Connected dat's textbox and send button to        greetingcontroller. However function connect() within message-box.html no longer prints to page and idk how to fix it yet

--- src/main/java/edu/csupomona/cs480/GreetingController.java
+++ src/main/java/edu/csupomona/cs480/GreetingController.java
@@ -4,21 +4,15 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import edu.csupomona.cs480.data.User;
+
 @Controller
 public class GreetingController {
-	private String userName;
+	private String userName = "TempUser";
 	private String receivedMessage;
 	
-	//stompClient connected over socket in html file.
-	//calls stompClient.send("/app/userName").
-	//stompClient is subscribed to topic/greetings and will pick up the return
-	//object
-    @MessageMapping("/userName")
-    @SendTo("/topic/greetings")
-    public Greeting userName(HelloMessage message) throws Exception {
-    	userName = message.getName();
-        return new Greeting("Username is: " + userName +".");
-    }
+	//User userObject = new User();
+	//userName = userObject.getName();
     
     //stompClient connected over socket in html file.
     //calls stompClient.send("/app/userMessage") .
@@ -28,6 +22,9 @@ public class GreetingController {
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
         receivedMessage = message.getMessage();
+        
+        System.out.println("Message is: " + receivedMessage);
+        
         return new Greeting(userName + " message: " + receivedMessage);
     } 
     
