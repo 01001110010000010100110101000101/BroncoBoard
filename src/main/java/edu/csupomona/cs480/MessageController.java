610949afea8d3482920e2cb58efd573ff77d364e
cheripai <datdo@rocketmail.com>
2015-11-02 16:29:20 -0800

    Add ability to post messages to chat box

--- src/main/java/edu/csupomona/cs480/MessageController.java
+++ src/main/java/edu/csupomona/cs480/MessageController.java
@@ -11,7 +11,6 @@ public class MessageController {
     @MessageMapping("/send/cs480")
     @SendTo("/receive/cs480")
     public Message passMessage(Message message) throws Exception {
-        System.out.println(message.getName() + "'s message is: " + message.getMessage());
         return message;
     } 
 
