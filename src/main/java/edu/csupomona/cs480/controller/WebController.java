5f1e82f5ead5d42cecf60cb56814b31288ec889f
cheripai <datdo@rocketmail.com>
2015-11-09 15:48:02 -0800

    Remove unnecessary functions

--- src/main/java/edu/csupomona/cs480/controller/WebController.java
+++ src/main/java/edu/csupomona/cs480/controller/WebController.java
@@ -20,109 +20,12 @@ import edu.csupomona.cs480.data.User;
 import edu.csupomona.cs480.data.provider.UserManager;
 
 
-/**
- * This is the controller used by Spring framework.
- * <p>
- * The basic function of this controller is to map
- * each HTTP API Path to the correspondent method.
- *
- */
-
 @RestController
 public class WebController {
 
-	/**
-	 * When the class instance is annotated with
-	 * {@link Autowired}, it will be looking for the actual
-	 * instance from the defined beans.
-	 * <p>
-	 * In our project, all the beans are defined in
-	 * the {@link App} class.
-	 */
     @Autowired
     private UserManager userManager;
 
-    /**
-     * This is a simple example of how to use a data manager
-     * to retrieve the data and return it as an HTTP response.
-     * <p>
-     * Note, when it returns from the Spring, it will be
-     * automatically converted to JSON format.
-     * <p>
-     * Try it in your web browser:
-     * 	http://localhost:8080/cs480/user/user101
-     */
-    @RequestMapping(value = "/cs480/user/{userId}", method = RequestMethod.GET)
-    User getUser(@PathVariable("userId") String userId) {
-    	User user = userManager.getUser(userId);
-        return user;
-    }
-
-    /**
-     * This is an example of sending an HTTP POST request to
-     * update a user's information (or create the user if not
-     * exists before).
-     *
-     * You can test this with a HTTP client by sending
-     *  http://localhost:8080/cs480/user/user101
-     *  	name=John major=CS
-     *
-     * Note, the URL will not work directly in browser, because
-     * it is not a GET request. You need to use a tool such as
-     * curl.
-     *
-     * @param id
-     * @param name
-     * @param major
-     * @return
-     */
-    @RequestMapping(value = "/cs480/user/{userId}", method = RequestMethod.POST)
-    User updateUser(
-    		@PathVariable("userId") String id,
-    		@RequestParam("name") String name,
-    		@RequestParam(value = "major", required = false) String major) {
-    	User user = new User();
-    	user.setId(id);
-    	user.setMajor(major);
-    	user.setName(name);
-    	userManager.updateUser(user);
-    	return user;
-    }
-
-    /**
-     * This API deletes the user. It uses HTTP DELETE method.
-     *
-     * @param userId
-     */
-    @RequestMapping(value = "/cs480/user/{userId}", method = RequestMethod.DELETE)
-    void deleteUser(
-    		@PathVariable("userId") String userId) {
-    	userManager.deleteUser(userId);
-    }
-
-
-
-    /**
-     * This API lists all the users in the current database.
-     *
-     * @return
-     */
-    @RequestMapping(value = "/cs480/users/list", method = RequestMethod.GET)
-    List<User> listAllUsers() {
-    	return userManager.listAllUsers();
-    }
-
-    /*********** Web UI Test Utility **********/
-    /**
-     * This method provide a simple web UI for you to test the different
-     * functionalities used in this web service.
-     */
-    @RequestMapping(value = "/cs480/home", method = RequestMethod.GET)
-    ModelAndView getUserHomepage() {
-        ModelAndView modelAndView = new ModelAndView("home");
-        modelAndView.addObject("users", listAllUsers());
-        return modelAndView;
-    }
 
     /* This method lists all classes available */
     @RequestMapping(value = "/classes", method = RequestMethod.GET, produces = "application/JSON")
@@ -136,7 +39,6 @@ public class WebController {
     	}
     }
     
-    
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     ModelAndView getHelp() {
         ModelAndView models = new ModelAndView("login");
@@ -148,14 +50,12 @@ public class WebController {
         ModelAndView models = new ModelAndView("chat");
         return models;
     }
-   
 
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
     ModelAndView getSample() {
         ModelAndView model = new ModelAndView("registration");
         return model;
     }
-    
 
     @RequestMapping(value = "/success", method = RequestMethod.GET)
     ModelAndView getTest() {
